3d23a84b9874f5ba40d440328ef59aaa7d1e1150
dbrackovic2 <dbrackovic2@etf.unsa.ba>
2022-12-22 21:10:00 +0100

    section 18 wrap up

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -4,7 +4,7 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 exports.handler = async (e) => {
     try {
         const { amount } = JSON.parse(e.body);
-        const paymentIntent = await stripe.paymentIntent.create({
+        const paymentIntent = await stripe.paymentIntents.create({
             amount,
             currency: "usd",
             payment_method_types: ["card"]
@@ -13,12 +13,12 @@ exports.handler = async (e) => {
         return {
             statusCode: 200,
             body: JSON.stringify({ paymentIntent })
-        }
+        };
     } catch (error) {
         console.log(error);
         return {
             status: 400,
             body: JSON.stringify({ error })
-        }
+        };
     }
 };
