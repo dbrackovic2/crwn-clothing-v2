08bb7408ac34227be461a645a372facd44084143
dbrackovic2 <dbrackovic2@etf.unsa.ba>
2023-01-11 02:06:39 +0100

    migrate payment form to ts

--- src/components/payment-form/payment-form.component.tsx
+++ src/components/payment-form/payment-form.component.tsx
@@ -1,10 +1,15 @@
-import { useState } from "react";
+import { useState, FormEvent } from "react";
 import { useSelector } from "react-redux";
 import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import { StripeCardElement } from "@stripe/stripe-js";
 import { selectCartTotal } from "../../store/cart/cart.selector";
 import { selectCurrentUser } from "../../store/user/user.selector";
 import {BUTTON_TYPE_CLASSES} from "../button/button.component";
 import { PaymentFormContainer, FormContainer, PaymentButton } from "./payment-form.styles";
+
+const ifValidCardElement = (
+    card: StripeCardElement | null
+): card is StripeCardElement => card !== null;
 
 export const PaymentForm = () => {
     const stripe = useStripe();
@@ -13,7 +18,7 @@ export const PaymentForm = () => {
     const currentUser = useSelector(selectCurrentUser);
     const [isProcessingPayment, setIsProcessingPayment] = useState(false);
 
-    const paymentHandler = async (e) => {
+    const paymentHandler = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         if(!stripe || !elements) return;
 
@@ -29,9 +34,13 @@ export const PaymentForm = () => {
 
         const clientSecret = response.paymentIntent.client_secret;
 
+        const cardDetails = elements.getElement(CardElement);
+
+        if(!ifValidCardElement(cardDetails)) return;
+
         const paymentResult = await stripe.confirmCardPayment(clientSecret, {
             payment_method: {
-                card: elements.getElement(CardElement),
+                card: cardDetails,
                 billing_details: {
                     name: currentUser ? currentUser.email : 'Guest user'
                 }
