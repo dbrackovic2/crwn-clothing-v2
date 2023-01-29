705fedabef5d5a9554ebbd88b46b552e1398866a
dbrackovic2 <dbrackovic2@etf.unsa.ba>
2023-01-29 18:32:55 +0100

    added mobile styling

--- src/components/directory-item/directory-item.styles.tsx
+++ src/components/directory-item/directory-item.styles.tsx
@@ -69,4 +69,7 @@ export const DirectoryItemContainer = styled.div`
     &:last-child {
         margin-left: 7.5px;
     }
+    @media screen and (max-width: 800px) {
+        height: 200px;
+    }
 `
--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Elements } from '@stripe/react-stripe-js';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import './index.scss';
 
 import App from './App';
 import { store, persistor } from './store/store';
@@ -30,5 +29,5 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals();
+reportWebVitals();
 serviceWorkerRegistration.register();
